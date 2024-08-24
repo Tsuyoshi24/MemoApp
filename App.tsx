6c1d729969890e7acae1c47e0b98f646debb8b6f
tsuyoshi <k2b4.t4k4@gmail.com>
2024-08-24 18:47:59 +0900

    add new file hellp.txt

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import Hello from './src/components/Hello'
 
 const App = ():JSX.Element => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!^^</Text>
+      <Text>hello i am tsucyako</Text>
+      <Hello >world</Hello>
+      <Hello style={{fontSize: 100}}>world</Hello>
       <StatusBar style="auto" />
     </View>
   )
