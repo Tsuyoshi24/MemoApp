f8df482e48cb333232a90d1ae70908f57d5184c2
tsuyoshi <k2b4.t4k4@gmail.com>
2024-08-26 20:00:32 +0900

    add new file and activate expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native'
+
+const Index = () => {
+
+    return (
+        <View>
+            <Text>
+                Hello World!!
+            </Text>
+        </View>
+    )
+}
+
+export default Index
